2cc45377f535b58bc8da8e71c9628bb0bcf4a9b1
Zhi Yang <zhiyang@usc.edu>
2020-07-11 08:15:54 -0700

    add token and keys.

--- script.R
+++ script.R
@@ -1,6 +1,17 @@
 library(tweetrmd)
 library(rtweet)
 
+epitwitter_token <- function() {
+  rtweet::create_token(
+    "rtweet_tokens_zy",
+    consumer_key = Sys.getenv("consumer_key"),
+    consumer_secret = Sys.getenv("consumer_secret"),
+    access_token = Sys.getenv("access_token"),
+    access_secret = Sys.getenv("access_secret"),
+    set_renv = FALSE
+  )
+}
+
 handle <- "zhiiiyang"
 recent_tweets <- get_timeline(handle, n = 1)
 
