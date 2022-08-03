5c02fae04c8d5e55944b9fa4a20f60cac43a6ce5
zhiiiyang <zyang895@gmail.com>
2022-08-02 22:40:39 -0700

    update tweet_url

--- script.R
+++ script.R
@@ -1,21 +1,19 @@
 library(tweetrmd)
 library(rtweet)
 
-lasttweet_token <- function() {
-  rtweet_bot(
-    api_key = Sys.getenv("CONSUMER_KEY"),
-    api_secret = Sys.getenv("CONSUMER_SECRET"),
-    access_token = Sys.getenv("ACCESS_TOKEN"),
-    access_secret = Sys.getenv("ACCESS_SECRET")
-  )
-}
+rtweet_bot(
+  api_key = Sys.getenv("CONSUMER_KEY"),
+  api_secret = Sys.getenv("CONSUMER_SECRET"),
+  access_token = Sys.getenv("ACCESS_TOKEN"),
+  access_secret = Sys.getenv("ACCESS_SECRET")
+)
 
 handle <- "zhiiiyang"
-recent_tweets <- get_timeline(handle, n = 1, token = lasttweet_token())
+recent_tweets <- get_timeline(handle, n = 1)
 
 tmpimg <- "tweet.png"
 tweet_screenshot(
-  tweet_url(handle, recent_tweets$status_id),
+  tweet_url(handle, recent_tweets$id_str),
   scale = 5, 
   maxwidth = 600,
   theme = "dark",
